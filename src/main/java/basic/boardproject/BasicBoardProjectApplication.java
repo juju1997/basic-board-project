36af341da95a9ba3f6f62729e88cccfb31ce3a4e
주형진 <57661869+juju1997@users.noreply.github.com>
2023-12-05 23:40:21 +0900

    #6 - 스프링 부트 프로젝트 시작
    스프링 부트 프로젝트 초기화
    * 스프링 웹
    * 액츄에이터
    * 데브툴즈
    * 롬복

---
+++ src/main/java/basic/boardproject/BasicBoardProjectApplication.java
@@ -0,0 +1,13 @@
+package basic.boardproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicBoardProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicBoardProjectApplication.class, args);
+    }
+
+}
