3e328df21542a3d7eefd57ff8ef5d4051192e9de
주형진 <gudwls671214@gmail.com>
2023-12-10 23:05:15 +0900

    #1 - Data REST 테스트
    Rest API 테스트 클래스 작성

--- src/main/java/basic/boardproject/domain/Article.java
+++ src/main/java/basic/boardproject/domain/Article.java
@@ -4,13 +4,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.LocalDateTime;
 import java.util.LinkedHashSet;
 import java.util.Objects;
 import java.util.Set;
