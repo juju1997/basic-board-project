a59565d4c22836faecc5ed88f18bff2b3f45ebf4
주형진 <gudwls671214@gmail.com>
2023-12-14 23:30:35 +0900

    #20 - 댓글 서비스 로직의 테스트와 골격 잡기
    댓글과 관련된 단순 CRUD 기능을 테스트로 표현

---
+++ src/main/java/basic/boardproject/service/ArticleCommentService.java
@@ -0,0 +1,25 @@
+package basic.boardproject.service;
+
+import basic.boardproject.dto.ArticleCommentDto;
+import basic.boardproject.repository.ArticleCommentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class ArticleCommentService {
+
+    private final ArticleCommentRepository articleCommentRepository;
+
+    @Transactional(readOnly = true)
+    public List<ArticleCommentDto> searchArticleComment(Long articleId) {
+        return List.of();
+    }
+
+    public void saveArticleComment(ArticleCommentDto dto) {
+    }
+}
